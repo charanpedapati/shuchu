efa8f0a61d977b161f1ba5c3874a88d985d21301
Mona Lisa <charanpedapati.io@gmail.com>
2025-04-18 21:53:10 +0530

    new commit lol

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ import Timer from './components/Timer'
 function App() {
   return (
     <>
-    <h1>集中 (Shūchū) | Pomodoro Timer </h1>
-    <h3>Time is running out, Please Shūchū 🍅</h3>
+    <h1 className='header'>集中 (Shūchū) | Pomodoro Timer </h1>
+    <h3>😳Sensei, Your time in this world is running out!</h3>
+    <h4>🥷You have to 集中 (Shūchū)</h4>
     <Timer></Timer>
     </>
   )
